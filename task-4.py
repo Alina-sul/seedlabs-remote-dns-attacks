1ce68b3aba91a14bd6605f69d3bf27f1695d3681
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-04-22 15:17:59 +0300

    task 4

---
+++ task-4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+from scapy.all import *
+
+# Variables
+client_ip = '10.0.2.17'
+server_ip = '10.0.2.18'
+attacker_ip = '10.0.2.19'
+
+# DNS Query Section
+Qdsec = DNSQR(qname='www.example.com')
+
+# DNS Header
+dns = DNS(id=0xAAAA, qr=0, qdcount=1, ancount=0, nscount=0, arcount=0, qd=Qdsec)
+
+# IP Header
+ip = IP(dst=server_ip, src=attacker_ip)
+
+# UDP Header
+udp = UDP(dport=53, sport=33333, chksum=0)
+
+# Complete Request
+request = ip/udp/dns
+
+# Send the request
+send(request)
