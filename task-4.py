618ad156ab834d4f05159e068b87cd3fd3a983e8
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2023-04-22 20:00:37 +0300

    task-4 fix

--- task-4.py
+++ task-4.py
@@ -13,10 +13,10 @@ Qdsec = DNSQR(qname='www.example.com')
 dns = DNS(id=0xAAAA, qr=0, qdcount=1, ancount=0, nscount=0, arcount=0, qd=Qdsec)
 
 # IP Header
-ip = IP(dst=server_ip, src=attacker_ip)
+ip = IP(dst=server_ip, src=client_ip)
 
 # UDP Header
-udp = UDP(dport=53, sport=33333, chksum=0)
+udp = UDP(dport=7070, sport=33333, chksum=0)
 
 # Complete Request
 request = ip/udp/dns
